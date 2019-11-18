bc2991c182283f7dda7503979b86573ac64518aa
Nicholas <xlorex50@gmail.com>
2019-11-19 01:17:52 +0800

    adding approute for cart

--- SimpleApplication/SimpleApplication.py
+++ SimpleApplication/SimpleApplication.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 
 @app.route('/contactUs')
 def contactUs():
